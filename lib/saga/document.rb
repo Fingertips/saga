6afccba71a7f1d5e8c9c98e373b67a8788731b5f
Manfred Stienstra <manfred@fngtps.com>
2010-04-02 10:41:45 +0200

    Make it possible to autofill IDs in a document.

--- lib/saga/document.rb
+++ lib/saga/document.rb
@@ -11,5 +11,35 @@ module Saga
       @stories      = ActiveSupport::OrderedHash.new
       @definitions  = ActiveSupport::OrderedHash.new
     end
+    
+    def used_ids
+      @stories.values.inject([]) do |ids, stories|
+        ids.concat stories.map { |story| story[:id] }
+        ids
+      end.compact
+    end
+    
+    def unused_ids(limit)
+      position = 1
+      used_ids = used_ids()
+      (1..limit).map do
+        while used_ids.include?(position) do position += 1 end
+        used_ids << position
+        position
+      end
+    end
+    
+    def length
+      stories.inject(0) { |total, (_, stories)| total + stories.length }
+    end
+    
+    def autofill_ids
+      unused_ids = unused_ids(length - used_ids.length)
+      stories.each do |_, stories|
+        stories.each do |story|
+          story[:id] ||= unused_ids.shift
+        end
+      end
+    end
   end
 end
--- test/saga_document_spec.rb
+++ test/saga_document_spec.rb
@@ -25,4 +25,61 @@ describe "A Document" do
     end
     document.definitions.keys.should == sections
   end
+  
+  it "should return a list of used IDs" do
+    document = Saga::Document.new
+    document.used_ids.should == []
+    
+    document.stories[''] = []
+    document.stories[''] << { :id => 2 }
+    document.used_ids.should == [2]
+    
+    document.stories['Non-functional'] = []
+    document.stories['Non-functional'] << { :id => 12 }
+    document.used_ids.should == [2, 12]
+    
+    document.stories['Non-functional'] << { :id => 3 }
+    document.used_ids.should == [2, 12, 3]
+  end
+  
+  it "should return a list of unused IDs" do
+    document = Saga::Document.new
+    document.unused_ids(0).should == []
+    document.unused_ids(3).should == [1,2,3]
+    document.unused_ids(4).should == [1,2,3,4]
+    
+    document.stories[''] = []
+    document.stories[''] << { :id => 2 }
+    document.stories[''] << { :id => 4 }
+    document.stories[''] << { :id => 5 }
+    document.stories[''] << { :id => 6 }
+    document.stories[''] << { :id => 100 }
+    
+    document.unused_ids(0).should == []
+    document.unused_ids(1).should == [1]
+    document.unused_ids(2).should == [1,3]
+    document.unused_ids(3).should == [1,3,7]
+    document.unused_ids(4).should == [1,3,7,8]
+  end
+  
+  it "should autofills ids" do
+    document = Saga::Document.new
+    document.autofill_ids
+    
+    document.stories[''] = []
+    document.stories[''] << { :description => 'First story'}
+    document.stories[''] << { :description => 'Second story'}
+    
+    document.stories['Non-functional'] = []
+    document.stories['Non-functional'] << { :id => 1, :description => 'Third story' }
+    
+    document.stories['Developer'] = []
+    document.stories['Developer'] << { :description => 'Fourth story' }
+    document.stories['Developer'] << { :id => 3, :description => 'Fifth story' }
+    
+    document.autofill_ids
+    document.stories[''].map { |story| story[:id] }.should == [2, 4]
+    document.stories['Non-functional'].map { |story| story[:id] }.should == [1]
+    document.stories['Developer'].map { |story| story[:id] }.should == [5, 3]
+  end
 end
