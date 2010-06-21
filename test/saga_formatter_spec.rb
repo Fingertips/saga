dc1eefb912c0307ac92a5ec5429e2a19d0852057
Manfred Stienstra <manfred@fngtps.com>
2010-06-21 12:04:25 +0200

    Render the introduction text when it's present.

--- test/saga_formatter_spec.rb
+++ test/saga_formatter_spec.rb
@@ -4,6 +4,18 @@ describe "Formatter" do
   before do
     @document = Saga::Document.new
     @document.title = 'Requirements API'
+    @document.authors = [
+      {:name => 'Thijs van der Vossen', :email => 'thijs@fngtps.com', :company => 'Fingertips', :website => 'http://www.fngtps.com'},
+      {:name => 'Manfred Stienstra', :email => 'manfred@fngtps.com', :company => 'Fingertips', :website => 'http://www.fngtps.com'}
+    ]
+    @document.introduction = [
+      'A web service for interfacing with the service.', 'Exposes a public API to the application.'
+    ]
+    @document.stories = [
+      ['General', [
+        {:description => 'As a consumer I would like to use TLS (SSL) so that my connection with the API is secure', :id => 4, :status => 'todo' }
+      ]]
+    ]
   end
   
   it "formats a saga document to HTML" do
